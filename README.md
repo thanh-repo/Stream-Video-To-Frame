93d8711f101cafbdd726ccc641c26ed935ed11a3
Nguyen Thanh Dat <thanhdat6716@gmail.com>
2022-08-29 16:48:53 +0900

    Add func save video file

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 ## Run file: Exe\StreamToFrame.exe
 ## Config file: Exe\config.json
 # config content:
-- rtsp: rtsp video url
-- path: path to save frame file
-- interval: frame save interval in milliseconds.
-
+- rtsp: rtsp video url (rtsp://admin:Password1!@180.83.55.124/profile2/media.smp)
+- ImagePath: path to save frame file (D:\StreamToFrame\Images)
+- VideoPath: path to save AVI video file (D:\StreamToFrame\Videos)
+- interval: frame save interval in milliseconds (1000)
+- "SaveImage": is enabled to save frame file (true)
+- "SaveVideo": is enabled to save video file (true)
 ## How to use:
 Open run file: Exe\StreamToFrame.exe
--- StreamToFrame.py
+++ StreamToFrame.py
@@ -4,6 +4,7 @@ import json
 import pathlib
 import datetime
 import os
+import numpy as np
 
 os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 """
@@ -16,20 +17,43 @@ if __name__ == '__main__':
         with open("config.json", 'r') as fp:
             config = json.load(fp)
         rtsp = config["rtsp"]
-        save_path = config["path"]
-        interval = int(int(config["interval"]) / 1000)
-        pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
+        image_path = config["ImagePath"]
+        video_path = config["VideoPath"]
+        save_image = config["SaveImage"]
+        save_video = config["SaveVideo"]
+        interval = int(config["interval"])
+
+        pathlib.Path(image_path).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(video_path).mkdir(parents=True, exist_ok=True)
+
         cap = cv2.VideoCapture(rtsp, cv2.CAP_FFMPEG)
+
+        frame_width = int(cap.get(3))
+        frame_height = int(cap.get(4))
+
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        video_name = filename = os.path.join(video_path,
+                                             datetime.datetime.now().strftime("%Y%m%d-%H%M%S-%f")[:-3] + ".avi")
+        out = cv2.VideoWriter(video_name, fourcc, 30.0, (frame_width, frame_height))
+        starttime = time.time()
+        starttime_live = time.time()
         while True:
             result, image = cap.read()
             if result:
-                filename = os.path.join(save_path,
-                                        datetime.datetime.now().strftime("%Y%m%d-%H%M%S-%f")[:-3] + ".png")
-                cv2.imwrite(filename, image)
-                print(f"Saved to: {filename}")
+                datetime_now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S-%f")[:-3]
+                filename = os.path.join(image_path, datetime_now + ".png")
+                now = time.time()
+                if (now - starttime) * 1000 > interval:
+                    starttime = now
+                    if save_image is True:
+                        cv2.imwrite(filename, image)
+                if (now - starttime_live) * 1000 > 1000:
+                    starttime_live = now
+                    print(f"Datetime now: {datetime_now}")
+                if save_video is True:
+                    out.write(image)
             else:
                 print("Cannot read and save frame")
-            time.sleep(interval)
     except Exception as e:
         print("Cannot start program")
         print(e)
