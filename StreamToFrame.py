c81abf4984cd6c9d5ad33dd3e6b7a717b3c75df3
Nguyen Thanh Dat <thanhdat6716@gmail.com>
2022-08-29 14:17:31 +0900

    Update Exe

--- StreamToFrame.py
+++ StreamToFrame.py
@@ -8,7 +8,7 @@ import os
 os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
 """
 pip freeze > requirements.txt
-pyinstaller --onefile --clean -w --icon=icon.ico --hidden-import win32timezone Server.Api.Service.py
+pyinstaller --onefile --clean --icon=icon.ico StreamToFrame.py
 """
 
 if __name__ == '__main__':
@@ -17,17 +17,18 @@ if __name__ == '__main__':
             config = json.load(fp)
         rtsp = config["rtsp"]
         save_path = config["path"]
-        interval = int(int(config["interval"])/1000)
+        interval = int(int(config["interval"]) / 1000)
         pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
         cap = cv2.VideoCapture(rtsp, cv2.CAP_FFMPEG)
         while True:
             result, image = cap.read()
             if result:
-                filename = os.path.join(save_path, datetime.datetime.now().strftime("%Y-%m-%d.%H-%M-%S.%f")[:-3] + ".png")
+                filename = os.path.join(save_path,
+                                        datetime.datetime.now().strftime("%Y%m%d-%H%M%S-%f")[:-3] + ".png")
                 cv2.imwrite(filename, image)
-                print(filename)
+                print(f"Saved to: {filename}")
             else:
-                print("Cannot Read Frame")
+                print("Cannot read and save frame")
             time.sleep(interval)
     except Exception as e:
         print("Cannot start program")
